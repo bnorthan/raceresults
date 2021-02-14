0e9d831202f0e1f57d061d27f61d7d69a5abb23b
bnorthan <bnorthan@gmail.com>
2021-02-14 13:39:04 +0000

    Tweak settings so site works on pythonanywhere

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!bd-gg&%u7v0!uln*$q&zx@hbdyi&6v=ij2+aqdc9wbs=g@#dy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':str(BASE_DIR / 'db.sqlite3'),
     }
 }
 

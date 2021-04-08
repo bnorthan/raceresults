e000a568a0977507da5a577d4455258ebbfaea25
Brian Northan <bnorthan@gmail.com>
2021-04-08 13:10:10 -0400

    Add all races at once
    This script is now called to rebuild the table by adding all races,
    should probably consider creating one script to rebuild the table
    from scratch (ie add all races) and others to simply add new races.

--- src/scripts/makedjangotable.py
+++ src/scripts/makedjangotable.py
@@ -29,6 +29,9 @@ import header
 import readers
 reload(readers)
 
+from django.db.transaction import atomic
+
+@atomic
 def add_race(race_data, race_name, race_city,race_date):
     
     results=readers.parse_general(race_data, header.RaceHeader.headers, 1)
@@ -44,8 +47,19 @@ def add_race(race_data, race_name, race_city,race_date):
         duration=timedelta(seconds=seconds)
         
         result=Result(first_name=r['first_name'],last_name=r['last_name'],city=r.city, time=duration,race=race)
+        print(r.last_name)
         result.save()
-    
-#add_race(pd.read_csv('../../data/2020/VirtualStockadeFinal.csv'),race_name='Virtual Stockade-athon',race_city='Capitol Region', race_date='2020-11-15');
-#add_race(pd.read_csv('../../data/2019/Stockade2019.csv'), race_name='2019 Stockade-athon', race_city='Schenectedy', race_date='2019-11-10');
-add_race(pd.read_fwf('../../data/2018/stockade.txt'), race_name='2018 Stockade-athon', race_city='Schenectedy', race_date='2018-11-11');
+
+'''
+race=pd.read_csv('../../data/2020/VirtualStockadeFinal.csv')
+add_race(race,race_name='2020 Stockade-athon (Virtual)',race_city='Capitol Region', race_date='2020-11-15')
+race=pd.read_csv('../../data/2019/Stockade2019.csv')
+add_race(race, race_name='2019 Stockade-athon', race_city='Schenectedy', race_date='2019-11-10')
+race=pd.read_fwf('../../data/2018/stockade.txt')
+add_race(race, race_name='2018 Stockade-athon', race_city='Schenectedy', race_date='2018-11-11')
+race=pd.read_fwf('../../data/2017/2017stockade.txt')
+add_race(race, race_name='2017 Stockade-athon', race_city='Schenectedy', race_date='2017-11-12')
+'''
+
+race=pd.read_csv('../../data/2016/stockade.csv')
+add_race(race, race_name='2016 Stockade-athon', race_city='Schenectedy', race_date='2016-11-13')

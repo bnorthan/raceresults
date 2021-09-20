4ab864c83455d48b8631de4911effaff656600e5
Brian Northan <bnorthan@gmail.com>
2021-09-20 12:09:38 -0400

    Add URL and template for grand prix page

--- django/mysite/raceresults/urls.py
+++ django/mysite/raceresults/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('runners/', views.runners, name='runners'),
     #path('race/<int:race_id>/', views.race, name='race'),
     url(r'^race/(?P<race_id>[0-9]+)/$', views.race, name='race'),
+    url(r'^gp/(?P<race_id>[0-9]+)/$', views.gp, name='gp'),
     url(r'^runner/(?P<runner_id>[0-9]+)/$', views.runner, name='runner'),
     
 ]

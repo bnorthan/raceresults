f3cf88e9d9c1fe9ca16108fedbeeb2e67e9407fd
Brian Northan <bnorthan@gmail.com>
2021-12-17 11:20:11 -0500

    Start Jekyll Site
    Preliminary work on a Jekyll site.  Thus far we've just used github
    defaults but now we have a simple layout, Gemfile, and index.html and in
    the future can work towards a more customized Jekyll site.

--- scripts/makemarkdown.py
+++ scripts/makemarkdown.py
@@ -14,6 +14,14 @@ https://bnorthan.github.io/raceresults/results/2020/VirtualStockade
 """
 
 import pandas as pd
+import os
+
+def make_markdown(racename, results, outname):
+    markdown='## '+racename+'\n\n'
+    markdown+=results.to_markdown()
+    outfile=open(outname, "w")
+    outfile.write(markdown)
+    outfile.close()
 
 results = pd.read_csv('../data/2020/VirtualStockadeFinal.csv')
 racename='Virtual Stockade-athon'
@@ -22,8 +30,12 @@ outname='../results/2020/VirtualStockade.md'
 results=results.drop('Company', axis=1)
 results=results.drop('Team', axis=1)
 
-markdown='## '+racename+'\n\n'
-markdown+=results.to_markdown()
-outfile=open(outname, "w")
-outfile.write(markdown)
-outfile.close()
+results = pd.read_csv('../data/2021/Stockade/Stockade_parsed.csv')
+racename='Stockade-athon 2021'
+outdir = '../results/2021/'
+outname=outdir+'Stockade.md'
+
+if os.path.exists(outdir)==False:
+    os.makedirs(outdir)
+
+make_markdown(racename, results, outname)

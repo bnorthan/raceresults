c1aea515da9b940e24192493547ef61b7a0439cf
Brian Northan <bnorthan@gmail.com>
2022-01-14 11:07:42 -0500

    Fix errors in 1981, 2017 and 2018 results

--- scripts/events/Stockadeathonalltime.py
+++ scripts/events/Stockadeathonalltime.py
@@ -185,8 +185,10 @@ out_file = '/home/bnorthan/runnin/raceresults/data/All Stockade results/'+str(ye
 years=[2003,2004,2005,2006,2007,2008,2009,2010,2011]
 years=[2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
 
-years=[2019,2020,2021]
-method=3
+years=[1981]
+method=0
+encoding=None
+
 for year in years:
     
     if (method==3):
